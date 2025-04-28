963c26b64598b064930e45e8dc8595f2e096dfdf
Federico <94263998+federico894@users.noreply.github.com>
2025-04-28 16:56:45 -0300

    initial commit

---
+++ Proyecto_Final_Algoritmos/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ (Main Program File)
+*/
+using System;
+
+namespace Proyecto_Final_Algoritmos
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
