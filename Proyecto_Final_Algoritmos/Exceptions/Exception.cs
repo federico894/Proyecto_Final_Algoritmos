47ce749b19862c3055a3655b138e4b23f3120ab7
Federico <94263998+federico894@users.noreply.github.com>
2025-05-13 12:33:14 -0300

    añadir clases

---
+++ Proyecto_Final_Algoritmos/Exceptions/Exception.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fede
+ * Date: 13/5/2025
+ * Time: 12:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Runtime.Serialization;
+
+namespace Proyecto_Final_Algoritmos.Exceptions
+{
+	/// <summary>
+	/// Description of Exception.
+	/// </summary>
+	public class Exception : Exception
+	{
+		public Exception()
+		{
+		}
+
+	 	public Exception(string message) : base(message)
+		{
+		}
+
+		public Exception(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		// This constructor is needed for serialization.
+		protected Exception(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
---
+++ Proyecto_Final_Algoritmos/Models/Servicio.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fede
+ * Date: 13/5/2025
+ * Time: 12:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Proyecto_Final_Algoritmos.Models
+{
+	/// <summary>
+	/// Description of Servicio.
+	/// </summary>
+	public class Servicio
+	{
+		public Servicio()
+		{
+		}
+	}
+}
