8cd84f6b22f84ff94de3aaba99dfd08e80f01b27
Federico <94263998+federico894@users.noreply.github.com>
2025-05-14 19:58:38 -0300

    agregar

--- Proyecto_Final_Algoritmos/Models/Evento.cs
+++ Proyecto_Final_Algoritmos/Models/Evento.cs
@@ -1,22 +1,38 @@
-﻿/*
- * Created by SharpDevelop.
- * User: fede
- * Date: 13/5/2025
- * Time: 12:30
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
+using System.Collections;
 
-namespace Proyecto_Final_Algoritmos.Models
+namespace Proyecto_Final_Algoritmos
 {
-	/// <summary>
-	/// Description of Evento.
-	/// </summary>
 	public class Evento
 	{
-		public Evento()
+		private String nombre_cliente;
+		private int dni_cliente;
+		private DateTime fecha;
+		private String tipo_evento;
+		private ArrayList servicios;
+		private Encargado encargado;
+		private int costo_total;
+		private int senia; // No Ñ :c
+
+		public Evento(String nombre_cliente, int dni_cliente, DateTime fecha, String tipo, ArrayList servicios, Encargado encargado, int costo_total, int senia)
 		{
+			this.nombre_cliente = nombre_cliente;
+			this.dni_cliente = dni_cliente;
+			this.fecha = fecha;
+			this.tipo_evento = tipo;
+			this.servicios = servicios;
+			this.encargado = encargado;
+			this.costo_total = costo_total;
+			this.senia = senia;
+		}
+		
+		public ArrayList Servicios{
+			get {
+				return servicios;
+			}
+			set {
+				servicios = value;
+			}
 		}
 	}
 }
