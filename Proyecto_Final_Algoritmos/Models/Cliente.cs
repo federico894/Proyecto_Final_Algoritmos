4bf2eff9518f0a197a4c838a73e4454acf4ffd72
Federico <94263998+federico894@users.noreply.github.com>
2025-05-18 14:46:05 -0300

    agregar archivos a la solucion
    trata de usar el sharpdevelop porque crear un archivo de 0 no lo agrega al archivo .csproj y no aparece en el sharpdevelop

--- Proyecto_Final_Algoritmos/Models/Cliente.cs
+++ Proyecto_Final_Algoritmos/Models/Cliente.cs
@@ -5,38 +5,42 @@ using System.Collections;
 namespace Proyecto_Final
 {
 
-public class Cliente
-{
-    //Declaracion de variables para el constructor
-    private string nombre;
-    private int dni;
-    public string NameReserva
-    {//Creación de propiedad la cual utilizamos para asignarle valor a las variables privadas
-        get { return nombre; }//get, nos devuelve el valor de la variable
-        set { nombre = value; }//determinamos el valor de la variable cuando instanciemos
-    }
-    public int DniReserva
-    {
-        get { return dni; }
-        set { dni = value; }
-    }
-    public ArrayList listClientes = new ArrayList(); //Lista la cual almacenara cada cliente
-    public Cliente()
-    {//Creacion del objeto y sus atributos(definimos atributos con set, por eso el constructor esta vacio);
-    }
+	public class Cliente
+	{
+		// Declaracion de variables para el constructor
+		private String nombre;
+		private int dni;
+		
+    	// Creación de propiedad la cual utilizamos para asignarle valor a las variables privadas
+		public String Name {
+			get { return nombre; } //get, nos devuelve el valor de la variable
+			set { nombre = value; } //determinamos el valor de la variable cuando instanciemos
+		}
+    
+		public int Dni {
+			get { return dni; }
+			set { dni = value; }
+		}
+    	
+    	// Lista la cual almacenara cada cliente
+		public ArrayList listClientes = new ArrayList();
+		
+		public Cliente()
+		{
+			//Creacion del objeto y sus atributos(definimos atributos con set, por eso el constructor esta vacio);
+		}
 
-    public void NewReserva(Reserva reserva)
-    {
-        listClientes.Add(reserva);
-    }
+		public void NewReserva(Reserva reserva)
+		{
+			listClientes.Add(reserva);
+		}
 
-    public void MostrarDatosReserva()
-    {
-        foreach (Reserva r in listClientes)
-        {
-            Console.WriteLine("| Reserva a nombre de: " + NameReserva +" | DNI "+DniReserva+ " | Fecha de Reserva: " + r.diaReservado + "/" + r.mesReservado+" |");
-        }
-    }
+		public void MostrarDatosReserva()
+		{
+			foreach (Reserva r in listClientes) {
+				Console.WriteLine("| Reserva a nombre de: " + NameReserva + " | DNI " + DniReserva + " | Fecha de Reserva: " + r.diaReservado + "/" + r.mesReservado + " |");
+			}
+		}
 
-}
+	}
 }
