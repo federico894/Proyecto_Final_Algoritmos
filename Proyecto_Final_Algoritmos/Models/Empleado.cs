7d749dca2bc8d8bda62c11923ce7c3f377ecffe4
Federico <94263998+federico894@users.noreply.github.com>
2025-05-19 19:31:15 -0300

    subir empleado.cs

--- Proyecto_Final_Algoritmos/Models/Empleado.cs
+++ Proyecto_Final_Algoritmos/Models/Empleado.cs
@@ -2,13 +2,42 @@
 
 namespace Proyecto_Final_Algoritmos
 {
-	/// <summary>
-	/// Description of Empleado.
-	/// </summary>
 	public class Empleado
 	{
-		public Empleado()
+		private String nombre;
+		private String apellido;
+		private int nro_legajo;
+		private int salario;
+		private String tareaDesempeniar;
+		private int dni;
+
+		public Empleado(String nombre, String apellido, int dni, int nro_legajo, int salario, String tareaDesempeniar)
 		{
+			this.nombre = nombre;
+			this.apellido = apellido;
+			this.dni = dni;
+			this.nro_legajo = nro_legajo;
+			this.salario = salario;
+			this.tareaDesempeniar = tareaDesempeniar;
+		}
+		
+		// Todas las propiedades
+		public string Nombre {get{return nombre;} set{nombre = value;} }
+		public string Apellido {get{return apellido;} set{apellido = value;} }
+		public int Dni {get{return dni;} set{dni = value;} }
+		public int NroDeLegajo {get{return nro_legajo;} set{nro_legajo = value;} }
+		public int Salario {get{return salario;} set{salario = value;} }
+		public string TareaDesempeniar {get{return tareaDesempeniar;} set{tareaDesempeniar = value;} }
+		
+		public void MostrarInformación(){
+			Console.WriteLine("Nombre: {0} {1}", Nombre, Apellido);
+			
+			Console.WriteLine("Nombre: " + Nombre + " " + Apellido);
+			Console.WriteLine("Cargo: " + TareaDesempeniar + " ");
+			Console.WriteLine("Dni: " + Dni + " ");
+			Console.WriteLine("NroDeLegajo: " + NroDeLegajo + " ");
+			Console.WriteLine("Sueldo: " + Salario + " ");
+			Console.WriteLine("TareaDesempeniar: " + TareaDesempeniar + " ");
 		}
 	}
 }
--- Proyecto_Final_Algoritmos/Models/Encargado.cs
+++ Proyecto_Final_Algoritmos/Models/Encargado.cs
@@ -2,9 +2,6 @@
 
 namespace Proyecto_Final_Algoritmos
 {
-	/// <summary>
-	/// Description of Encargado.
-	/// </summary>
 	public class Encargado
 	{
 		public Encargado()
