b53068cbacd63bbdd7dd176627a7054632528bd7
Gonzalo Fernandez <161097592+f-gonza@users.noreply.github.com>
2025-05-20 17:47:15 -0300

    Clase servicio

--- Proyecto_Final_Algoritmos/Models/Servicio.cs
+++ Proyecto_Final_Algoritmos/Models/Servicio.cs
@@ -1,22 +1,27 @@
-﻿/*
- * Created by SharpDevelop.
- * User: fede
- * Date: 13/5/2025
- * Time: 12:32
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+﻿
 using System;
 
 namespace Proyecto_Final_Algoritmos
 {
-	/// <summary>
-	/// Description of Servicio.
-	/// </summary>
+
 	public class Servicio
 	{
-		public Servicio()
+		private string nombre_servicio;
+		private string descripcion_serv;	
+		
+		public Servicio(string nombre_servicio, string descripcion_serv)
 		{
+			this.nombre_servicio = nombre_servicio;
+			this.descripcion_serv = descripcion_serv;
+		}
+		
+		public string Nombre_servicio{
+			get{return nombre_servicio;}
+			set{nombre_servicio = value;}
+		}
+		public string Descripcion_serv{
+			get{return descripcion_serv;}
+			set{descripcion_serv = value;}
 		}
 	}
 }
